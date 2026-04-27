26b6702c83b6cd6b54c2789d5d11702f6bd90ae6
jeffgun99 <jeffrygaleon@gmail.com>
2026-04-27 19:01:55 +0700

    sidebar dan header update

---
+++ apps/dashboard/app/(tenant)/layout.tsx
@@ -0,0 +1,20 @@
+import { Sidebar } from "@/components/layouts/Sidebar";
+import { Header } from "@/components/layouts/Header";
+
+export default function TenantLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="p-8 flex-1 overflow-y-auto bg-background-deep">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
